25fce97b85a9aab9f8f835384b3c1c9c6c83a256
MarijnSt <marijn.stammeleer@gmail.com>
2025-07-07 19:09:10 +0200

    feat: player stats df in app

--- dribble_analysis.py
+++ dribble_analysis.py
@@ -18,24 +18,130 @@ with col1:
 with col2:
     dribbles_filter = st.number_input("Minimum dribbles", min_value=0, max_value=100, value=10, step=1)
 
-st.markdown(f"Your selected options: {minutes_played_filter}")
-st.markdown(f"Your selected options: {dribbles_filter}")
+# st.markdown(f"Your selected options: {minutes_played_filter}")
+# st.markdown(f"Your selected options: {dribbles_filter}")
 
 
 # Filter player stats
+@st.cache_data
 def filter_player_stats(df_player_stats, minutes_played_filter, dribbles_filter):
     df = df_player_stats.copy()
+
+    # Apply filters
     if minutes_played_filter:
         df = df[df["playing_time"] >= minutes_played_filter * 60]
     if dribbles_filter:
         df = df[df["attempted_dribbles"] >= dribbles_filter]
+
+    # Convert playing time to minutes for display
+    df["playing_time"] = df["playing_time"] / 60
+
+    # Return relevant columns
+    # df = df[]
+
     return df
 
 df_player_stats_filtered = filter_player_stats(df_player_stats, minutes_played_filter, dribbles_filter)
-
-
 st.write(f"Number of players: {len(df_player_stats_filtered)}")
-st.dataframe(df_player_stats_filtered)
 
-st.dataframe(df_dribbles)
-st.write(f"Number of dribbles: {len(df_dribbles)}")
+# Select a player
+st.write(f"Select a player to see their dribble analysis")
+selected_player = st.dataframe(
+    df_player_stats_filtered,
+    column_config={
+        "player_short_name": st.column_config.TextColumn(
+            "Player",
+            help="The name of the player"
+        ),
+        "team_name": st.column_config.TextColumn(
+            "Team",
+            help="The team of the player"
+        ),
+        "playing_time": st.column_config.NumberColumn(
+            "Playing time",
+            help="Number of minutes played by the player",
+            format="%.0f",
+        ),
+        "goals": st.column_config.NumberColumn(
+            "Goals",
+            help="The number of goals scored by the player",
+            format="%.0f",
+        ),
+        "assists": st.column_config.NumberColumn(
+            "Assists",
+            help="The number of assists by the player",
+            format="%.0f",
+        ),
+        "shots": st.column_config.NumberColumn(
+            "Shots",
+            help="The number of shots taken by the player",
+            format="%.0f",
+        ),
+        "shots_xg": st.column_config.NumberColumn(
+            "Shots xG",
+            help="Total xG from shots taken by the player",
+            format="%.4f",
+        ),
+        "attempted_dribbles": st.column_config.NumberColumn(
+            "Attempted dribbles",
+            help="The total number of dribbles attempted by the player",
+            format="%.0f",
+        ),
+        "completed_dribbles": st.column_config.NumberColumn(
+            "Completed dribbles",
+            help="The number of completed dribbles by the player",
+            format="%.0f",
+        ),
+        "failed_dribbles": st.column_config.NumberColumn(
+            "Failed dribbles",
+            help="The number of failed dribbles by the player",
+            format="%.0f",
+        ),
+        "dribble_success_rate": st.column_config.NumberColumn(
+            "Dribble success rate",
+            help="The success rate of dribbles by the player",
+            format="%.2f",
+        ),
+        "danger_dribbles": st.column_config.NumberColumn(
+            "Danger dribbles",
+            help="Danger dribbles lead to a shot within 15 seconds",
+            format="%.0f",
+        ),
+        "danger_dribbles_xg": st.column_config.NumberColumn(
+            "Danger dribbles xG",
+            help="Total xG from shots that were taken after a danger dribble",
+            format="%.4f",
+        ),
+        "dribbles_to_goals": st.column_config.NumberColumn(
+            "Dribbles to goals",
+            help="The number of dribbles that lead to a goal",
+            format="%.0f",
+        ),
+        "xg_per_danger_dribble": st.column_config.NumberColumn(
+            "xG per danger dribble",
+            help="Average xG per danger dribble",
+            format="%.4f",
+        ),
+    },
+    column_order=[
+        "player_short_name", "team_name", "playing_time",
+        "goals", "assists", "shots", "shots_xg",
+        "attempted_dribbles", "completed_dribbles", "failed_dribbles", "dribble_success_rate",
+        "danger_dribbles", "danger_dribbles_xg", "dribbles_to_goals", "xg_per_danger_dribble"
+    ],
+    hide_index=True,
+    selection_mode="single-row",
+    on_select="rerun"
+)
+
+if not selected_player["selection"]["rows"]:
+    st.info("Select a player to proceed with the analysis.")
+    st.stop()
+
+# Get dribbles for the selected player
+selected_id = selected_player["selection"]["rows"][0]
+selected_player_id = df_player_stats_filtered.iloc[selected_id]["player_id"]
+st.write(f"Selected player: {selected_player_id}")
+
+# st.dataframe(df_dribbles)
+# st.write(f"Number of dribbles: {len(df_dribbles)}")
